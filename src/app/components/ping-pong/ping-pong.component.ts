fa23709e84d869b72ab0e1d77acb3ff6cd9bc17a
mennolodder <7911897+mennolodder@users.noreply.github.com>
2022-12-07 17:11:44 +0100

    Bal moves

--- src/app/components/ping-pong/ping-pong.component.ts
+++ src/app/components/ping-pong/ping-pong.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-ping-pong',
   templateUrl: './ping-pong.component.html',
-  styleUrls: ['./ping-pong.component.scss']
+  styleUrls: ['./ping-pong.component.scss'],
 })
-export class PingPongComponent implements OnInit {
+export class PingPongComponent implements AfterViewInit {
+  @ViewChild('bal')
+  bal!: ElementRef<HTMLDivElement>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  balTop = 200;
+  balLeft = 200;
+  ngAfterViewInit(): void {
+    const i = setInterval(() => {
+      this.moveBal();
+    }, 10);
   }
 
+  moveBal(): void {
+    this.balTop = this.balTop + 1;
+    this.bal.nativeElement.style.top = this.balTop.toString() + 'px';
+    this.balLeft = this.balLeft + 1;
+    this.bal.nativeElement.style.left = this.balLeft.toString() + 'px';
+  }
 }
