1f7e0adf0b9deafa660e2b097bbf889a57ba7967
mennolodder <7911897+mennolodder@users.noreply.github.com>
2022-12-24 13:16:36 +0100

    stuitert aan alle kanten

--- src/app/components/ping-pong/ping-pong.component.ts
+++ src/app/components/ping-pong/ping-pong.component.ts
@@ -13,6 +13,7 @@ export class PingPongComponent implements AfterViewInit {
   balLeft = 200;
 
   balOmlaag = false;
+  balNaarRechts = true;
 
   ngAfterViewInit(): void {
     const i = setInterval(() => {
@@ -32,18 +33,34 @@ export class PingPongComponent implements AfterViewInit {
       this.balTop = this.balTop - 1;
     }
 
-    //this.balLeft = this.balLeft + 1;
+    if (this.balNaarRechts) {
+      this.balLeft = this.balLeft + 1;
+    } else {
+      this.balLeft = this.balLeft - 1;
+    }
 
     this.bal.nativeElement.style.top = this.balTop.toString() + 'px';
     this.bal.nativeElement.style.left = this.balLeft.toString() + 'px';
   }
 
   private stuiteren() {
+    // boven
     if (this.balTop < 0) {
       this.balOmlaag = true;
     }
+
+    //onder
     if (this.balTop > 400 - 25) {
       this.balOmlaag = false;
+    }
+
+    //rechts
+    if (this.balLeft > 500 - 25) {
+      this.balNaarRechts = false;
+    }
+    if (this.balLeft < 0) {
+      console.log('stuiter');
+      this.balNaarRechts = true;
     }
   }
 }
